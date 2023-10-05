1378e0a3e3444f353fd8c19e85a9db76dbb1cda8
Hiroki Akahoshi <akaho1421@gmail.com>
2023-10-05 17:59:05 +0900

    update: 階段検証用のファイルを作成

---
+++ scripts/blockData/stair/checkStair.ts
@@ -0,0 +1,36 @@
+function checkStair(data: Record<string, string | number | boolean>) {
+  // x+
+  if (!data.upside_down_bit && data.weirdo_direction === 1) {
+    return 4.0;
+  }
+
+  // x-
+  if (!data.upside_down_bit && data.weirdo_direction === 0) {
+    return 4.1;
+  }
+
+  // z+
+  if (!data.upside_down_bit && data.weirdo_direction === 3) {
+    return 4.2;
+  }
+
+  // z-
+  if (!data.upside_down_bit && data.weirdo_direction === 2) {
+    return 4.3;
+  }
+
+  if (data.upside_down_bit && data.weirdo_direction === 1) {
+    return 4.4;
+  }
+  if (data.upside_down_bit && data.weirdo_direction === 0) {
+    return 4.5;
+  }
+  if (data.upside_down_bit && data.weirdo_direction === 3) {
+    return 4.6;
+  }
+  if (data.upside_down_bit && data.weirdo_direction === 2) {
+    return 4.7;
+  }
+}
+
+export { checkStair };
